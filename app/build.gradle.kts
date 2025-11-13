2c7120b80ebe1bc3ec8c4ad96ec71c6791bc302b
calvicii <lavoiethomas17@gmail.com>
2025-11-13 12:14:23 -0500

    Prepare 1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.onloc.android"
         minSdk = 31
         targetSdk = 36
-        versionCode = 106
-        versionName = "1.0.6"
+        versionCode = 107
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
