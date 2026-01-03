931e23fd21d382f37100b7774bc7bce2d450a2c3
calvicii <lavoiethomas17@gmail.com>
2026-01-03 12:26:23 -0500

    Bump to next version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.onloc.android"
         minSdk = 31
         targetSdk = 36
-        versionCode = 111
-        versionName = "1.1.1"
+        versionCode = 112
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
