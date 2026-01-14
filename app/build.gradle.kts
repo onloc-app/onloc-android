0deecb755d104e1e773ebe493c87aec03016916e
calvicii <lavoiethomas17@gmail.com>
2026-01-14 12:10:27 -0500

    Bump release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.onloc.android"
         minSdk = 31
         targetSdk = 36
-        versionCode = 112
-        versionName = "1.1.2"
+        versionCode = 113
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
